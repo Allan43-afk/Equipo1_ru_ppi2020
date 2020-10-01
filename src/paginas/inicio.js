0def3faa3651a832cffa619a50abd739eb611e59
AndersonArdila <66491641+AndersonArdila@users.noreply.github.com>
2020-10-01 13:20:05 -0500

    Cambio de boton

--- src/paginas/inicio.js
+++ src/paginas/inicio.js
@@ -7,43 +7,29 @@ class Inicio extends React.Component {
       <div className="container">
         <div className="row">
           <div className="col-sm-4 col-md-4 col-lg-4"></div>
-          <div className="col-sm-4 col-md-4 col-lg-4">
+          <div className="col-sm-4 col-md-4 col-lg-4 text-center">
+            <h1>MEJORAMIENTO DE LA CONVIVENCIA ESCOLAR</h1>
             <div className="row">
               <div className="col">
-                <center>
-                  {" "}
-                  <h1>MEJORAMIENTO DE LA CONVIVENCIA ESCOLAR</h1>{" "}
-                </center>
-              </div>
-              <div className="row">
-                <div className="col">
-                    {" "}
-                    <img
-                      src={ImagenInicio}
-                      ClassName="rounded mx-auto d-block imagenInicio"
-                      alt="..."
-                      height="200"
-                      width="580"
-                    />                  
-                </div>
+                <img
+                  src={ImagenInicio}
+                  ClassName="rounded mx-auto d-block imagenInicio"
+                  alt="..."
+                />
               </div>
             </div>
           </div>
         </div>
-
         <br />
         <br />
         <br />
-
         <div className="col">
-        <button
-              type="button"
-              class="btn btn-primary btn-lg btn-block boton43"
-            >
-                 <a href="./Tipodeusuario">Entrar</a>
-            </button>
-
-
+          <button
+            type="button"
+            class="btn btn-primary btn-lg btn-block boton43"
+          >
+            <a href="./Tipodeusuario">Entrar</a>
+          </button>
         </div>
       </div>
     );
