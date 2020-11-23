d18d8c92ba42bf4b34971f3ee8b3b5e78997f7b8
Femepe <66034704+Femepe@users.noreply.github.com>
2020-11-23 16:00:33 -0500

    Subiendo el apartado de Estudiante
    Enlace finalizado y aparentemente correcto

--- Backend/router/Estudiante.js
+++ Backend/router/Estudiante.js
@@ -5,7 +5,7 @@ const router = require("./routes");
 
 //nffs
 router.get('/estudiante', (req, res) => {
-    mysqlConnection.query('SELECT * FROM ESTUDIANTE', (err, rows, fields) => {
+    mysqlConnection.query('SELECT * FROM Estudiante', (err, rows, fields) => {
         //Si no hay error
         if (!err) {
             //Verdadero
@@ -17,4 +17,63 @@ router.get('/estudiante', (req, res) => {
     })//Fin query
 })//Fin del get
 
+//agregar estudiante nuevo
+
+router.post('/nuevo-estudiante',(req,res)=>{
+    const{identificacion, nombre_completo, fecha_nacimiento, correo_electronico, grado, genero, dirreccion, nombredel_acudiente, telefono_acudiente}=req.body;
+
+    let estudiante = [identificacion, nombre_completo, fecha_nacimiento, correo_electronico, grado, genero, dirreccion, nombredel_acudiente, telefono_acudiente];
+
+    let nuevoEstudiante = `INSERT INTO Estudiante (identificacion, nombre_completo, fecha_nacimiento, correo_electronico, grado, genero, dirreccion, nombredel_acudiente, telefono_acudiente)VALUES(?,?,?,?,?,?,?,?,?)`;
+
+    mysqlConnection.query(nuevoEstudiante,estudiante,(err,results,fields)=>{
+        if(err){
+            return console.error(err.message);
+        }else{
+            res.json({message:`Estudiante registrado exitosamente`});
+        }
+    });
+});
+
+router.put('/estudiantes/:identificacion', (req, res) => {
+    const {nombre_completo, fecha_nacimiento, correo_electronico, grado, genero, dirreccion, nombredel_acudiente, telefono_acudiente} = req.body;
+
+    const { identificacion } = req.params;
+
+    mysqlConnection.query(`UPDATE Estudiante SET nombre_completo = ?, fecha_nacimiento = ?, correo_electronico = ?, grado = ?, genero = ?, dirreccion = ?, nombredel_acudiente = ?, telefono_acudiente = ? WHERE identificacion = ?`,
+
+        [nombre_completo, fecha_nacimiento, correo_electronico, grado, genero, dirreccion, nombredel_acudiente, telefono_acudiente, identificacion], (err, rows, fields) => {
+            if (!err) {
+                res.json({ status: `estudiante Actualizado` });
+            } else {
+                console.log(err);
+            }
+        });
+});
+
+//bucar estudiante
+router.get('/estudiantes/:identificacion',(req,res)=>{
+    const {identificacion} = req.params;
+    mysqlConnection.query('SELECT * FROM Estudiante WHERE identificacion =?', [identificacion],(err,rows,fields)=>{
+        if(!err){
+            res.json(rows[0])
+        }else{
+            console.log(err);
+        }
+    })
+})
+
+//eliminar estudiantes
+router.delete('/estudiantes/:identificacion', (req,res) => {
+    const {identificacion} = req.params;
+    mysqlConnection.query('DELETE FROM Estudiante WHERE identificacion=?', [identificacion], (err, rows, fields) =>{
+        if(!err){
+            res.json({ status:'estudiante eliminado'});
+        }else{
+            console.log(err);
+        }
+    });
+});
+
 module.exports = router;
+
