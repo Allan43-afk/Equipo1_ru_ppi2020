f10ee8b1c332368e66b31637b54be321572fbada
ANDRES-MACHADO-O <66333549+ANDRES-MACHADO-O@users.noreply.github.com>
2020-11-25 18:16:33 -0500

    Registro de situacion
    Mitad del codigo, la otra mitad mi compañera

--- Backend/router/Registro_situacion.js
+++ Backend/router/Registro_situacion.js
@@ -1,10 +1,9 @@
 const express = require("express");
-const routes = express.Router();
+const router = express.Router();
 const mysqlConnection = require("../db/db");
-const router = require("./routes");
 
-router.get('/Registro_situacion', (req, res) => {
-    mysqlConnection.query('SELECT * FROM Registro_situacion', (err, rows, fields) => {
+router.get('/registrosituacion', (req, res) => {
+    mysqlConnection.query('SELECT * FROM RegistroSituacion', (err, rows, fields) => {
         //Si no hay error
         if (!err) {
             //Verdadero
@@ -16,58 +15,25 @@ router.get('/Registro_situacion', (req, res) => {
     })//Fin query
 })//Fin del get
 
+//agregar registro nuevo
 
-module.exports = router;
+router.post('/nueva-situacion',(req,res)=>{
+    const{Consecutivo, decripcion, fechareporte_sistema, id_usuario, estado, fecha_revision, identificacion_docente_revision, observaciones, Tipo}=req.body;
 
-//bucar
-router.get('/Registro_situacion/consecutivo',(req,res)=>{
-    const {consecutivo} = req.params; //consecutivo 
-    mysqlConnection.query('SELECT * FROM Registro_situacion WHERE consecutivo =?', [consecutivo],(err,rows,fields)=>{
-        if(!err){
-            res.json(rows[0])
-        }else{
-            console.log(err);
-        }
-    })
-}) // fin buscar
+    let registrosituacion = [Consecutivo, decripcion, fechareporte_sistema, id_usuario, estado, fecha_revision, identificacion_docente_revision, observaciones, Tipo];
 
-//actilizar
-router.put('/Registro_situacion/consecutivo', (req, res) => {
-    const { consecutivo, tipo, descripcion, fechareportesistema, idusuario, estado, fecharevision, identificaciondocenterevision, observaciones } = req.body;
-    const { consecutivo } = req.params;
-    mysqlConnection.query(`UPDATE Registro_situacion SET consecutivo=?, tipo=?, descripcion=?, fechareportesistema=?, idusuario=?, estado=?, fecharevision=?, identificaciondocenterevision=?, observaciones=?, WHERE consecutivo=?`,
-        [ consecutivo, tipo, descripcion, fechareportesistema, idusuario, estado, fecharevision, identificaciondocenterevision, observaciones], (err, rows, fields) => {
-            if (!err) {
+    let nuevoRegistro = `INSERT INTO RegistroSituacion (Consecutivo, decripcion, fechareporte_sistema, id_usuario, estado, fecha_revision, identificacion_docente_revision, observaciones, Tipo)VALUES(?,?,?,?,?,?,?,?,?)`;
 
-                res.json({ status: `Registro_situacion Actualizado` });
-            } else {
-                console.log(err);
-            }
-        })
-});
-//crear usuario
-router.post('/nuevo-Registro_situacion', (req, res) => {
-    const { consecutivo } = req.body;//1 Captura
-    let consecutivoArreglo = [consecutivo, tipo, descripcion, fechareportesistema, idusuario, estado, fecharevision, identificaciondocenterevision, observaciones];// Arreglo json
-    //Definir el scrip sql en una variable
-    let nuevoUsuario = 'SELECT * FROM Registro_situacion(modulo,mod) value(?,?)';
-    mysqlConnection.query(consecutivo, (err, results, fields) => {
-        //Si hay error
-        if (!err) {
-            //Verdadero
+    mysqlConnection.query(nuevoRegistro,registrosituacion,(err,results,fields)=>{
+        if(err){
             return console.error(err.message);
-        } else {//Si no
-            //Falso
-            res.json({ message: 'Reporte de situacion exitosa' });
-        }//Fin Si
-    })
-})//Fin guardar un usuario
-router.get('/Registro_situacion', (req, res) => {
-    mysqlConnection.query('SELECT * FROM Registro_situacion ', (err, rows, fiels) => {
-        if (!err) {
-            res.json(rows);
-        } else {
-            console.log(err);
+        }else{
+            res.json({message:`Situacion registrada exitosamente`});
         }
     });
-})// fin
+});
+//actualizar
+
+router.patch('/actualizar/:Consecutivo', (req, res) => {
+    const {decripcion, fechareporte_sistema, id_usuario, estado, fecha_revision, identificacion_docente_revision, observaciones, Tipo} = req.body;
+
